9ffb1a8d4f1c166c3f5adacfdff1fd761e99c423
“RaptorValley” <haha@nw63.onmicrosoft.com>
2025-02-21 20:10:51 +0800

    test

---
+++ src/class_definition_card.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifndef ___CLASS_DEFINITION_CARD_H
+#define ___CLASS_DEFINITION_CARD_H
+
+#include <iostream>
+
+class Card {
+  private:
+    std::string name; // 卡牌名称
+
+    enum Type { element,   // 元素
+                equipment, // 装置
+                prop,      // 道具
+    };
+
+    using D6 = unsigned int;
+    D6 requirement; // 至低点数
+
+    std::string edition; // 推出版本
+};
+
+#endif //___CLASS_DEFINITION_CARD_H
